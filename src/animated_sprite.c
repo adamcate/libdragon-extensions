2b830df237b0cba32d9ee6b9a7c675914a88d737
pepsiman3 <adamthomascate@gmail.com>
2022-11-15 21:20:17 -0700

    replace render code with newer rdpq API

--- src/animated_sprite.c
+++ src/animated_sprite.c
@@ -16,6 +16,7 @@ void animated_sprite_init_no_alloc(AnimatedSprite *anim, sprite_t *sprite, Size 
 								   Position render_offset, size_t offset_start, size_t offset_end,
 								   float anim_speed) {
 	anim->sprite = sprite;
+	anim->format = sprite_get_format(sprite);
 	anim->offset_start = offset_start;
 	anim->offset_end = offset_end;
 	anim->anim_speed = anim_speed;
@@ -41,9 +42,29 @@ void animated_sprite_tick(AnimatedSprite *anim, float anim_rate) {
 
 void animated_sprite_draw(AnimatedSprite *anim, Position pos, Rect screen_rect) {
 	if (is_intersecting(new_rect(pos, anim->size), screen_rect)) {
-		rdp_sync(SYNC_PIPE);
-		rdp_load_texture_stride(0, 0, MIRROR_DISABLED, anim->sprite, anim->_current_offset);
-		rdp_draw_sprite(0, pos.x - anim->render_offset.x, pos.y - anim->render_offset.y,
-						MIRROR_DISABLED);
+		
+		int tex_width = anim->sprite->width / anim->sprite->hslices;	// The width of the slice to load
+		int tex_height = anim->sprite->height / anim->sprite->vslices;	// The height of the slice to load
+
+		int s_0 = ((int)(anim->_current_offset) % anim->sprite->hslices) * tex_width;	// The leftmost s coordinate
+		int t_0 = ((int)(anim->_current_offset) / anim->sprite->hslices) * tex_height;	// The leftmost t coordinate
+		int s_1 = s_0 + tex_width - 1;	// Rightmost s coordinate
+		int t_1 = t_0 + tex_height - 1;	// Rightmost t coordinate
+
+		surface_t sprite_surf = sprite_get_pixels(anim->sprite);	// Set sprite_surf to point to the pixels of the sprite
+
+		rdpq_set_mode_copy(true);	// Switch to faster copy mode, since we aren't using mirroring
+
+		if(anim->format == FMT_CI4 || anim->format == FMT_CI8){	// if the image is paletted, load its color palette
+			rdpq_mode_tlut(TLUT_RGBA16);
+			rdpq_tex_load_tlut(sprite_get_palette(anim->sprite), 0, 16);
+		}
+
+		rdpq_tex_load_sub(TILE0, &sprite_surf, 0, s_0, t_0, s_1, t_1);
+
+		// draw the sprite slice to the screen
+		rdpq_texture_rectangle(TILE0, pos.x - anim->render_offset.x, pos.y - anim->render_offset.y,
+						pos.x - anim->render_offset.x + tex_width, pos.y - anim->render_offset.y + tex_height, s_0, t_0, 1.f, 1.f);
+		rdpq_mode_tlut(TLUT_NONE);	// Disable TLUT mode for the next sprite call
 	}
 }
