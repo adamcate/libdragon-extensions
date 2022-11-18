d911e398932ae16a00c3e6641fae7f36a1004808
pepsiman3 <adamthomascate@gmail.com>
2022-11-18 12:54:28 -0700

    convert tiled_cached_render to rdpq

--- src/tiled_cached.c
+++ src/tiled_cached.c
@@ -58,21 +58,38 @@ TiledCached *tiled_cached_init(MemZone *memory_pool, sprite_t *sprite, const cha
 	return tiled_map;
 }
 
-// Render a Tiled map
+// Render a cached Tiled map
 void tiled_cached_render(TiledCached *tiled, Rect screen_rect) {
-	rdp_sync(SYNC_PIPE);
+
+	surface_t tiled_surf = sprite_get_pixels(tiled->sprite);	// The surface pointing to the pixels of the tiled sprite 
+
+	int tex_width = tiled->sprite->width / tiled->sprite->hslices;	// The width of the slice to load
+	int tex_height = tiled->sprite->height / tiled->sprite->vslices;// The height of the slice to load
+
+	int s_0;	// leftmost s coordinate
+	int t_0;	// leftmost t coordinate
+	int s_1;	// rightmost s coordinate
+	int t_1;	// rightmost t coordinate
+
+	rdpq_mode_copy(true);
 
 	for (size_t i = 0; i < 255; ++i) {
 		if (tiled->tiles[i].count > 0) {
-			rdp_load_texture_stride(0, 0, MIRROR_DISABLED, tiled->sprite, i);
+			// calculate the s t coordinates of the slice of the parent sprite to use
+			s_0 = (i % tiled->sprite->hslices) * tex_width;
+			t_0 = (i % tiled->sprite->hslices) * tex_height;
+			s_1 = s_0 + tex_width - 1;
+			t_1 = s_1 + tex_height - 1;
+			// load the segment into memory
+			rdpq_load_sub(TILE0, &tiled_surf, 0, s_0, t_0, s_1, t_1);
+
+			// draw the segment at every cached coordinate instance
 			for (size_t j = 0; j < tiled->tiles[i].count; ++j) {
-				rdp_draw_textured_rectangle(
-					0, tiled->tiles[i].position[j].x * tiled->tile_size.width,
+				rdpq_texture_rectangle(TILE0, tiled->tiles[i].position[j].x * tiled->tile_size.width, 
 					tiled->tiles[i].position[j].y * tiled->tile_size.height,
 					tiled->tiles[i].position[j].x * tiled->tile_size.width + tiled->tile_size.width,
-					tiled->tiles[i].position[j].y * tiled->tile_size.height +
-						tiled->tile_size.height,
-					MIRROR_DISABLED);
+					tiled->tiles[i].position[j].y * tiled->tile_size.height + tiled->tile_size.height,
+					s_0, t_0, 1.f, 1.f);
 			}
 		}
 	}
